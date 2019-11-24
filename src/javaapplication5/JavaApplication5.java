f8eba2a61551ee8318fc1aa7b042d9963d727d07
TarasStroika <taras.peleshik@gmail.com>
2019-11-24 11:02:16 +0200

    Fifth commit

---
+++ src/javaapplication5/JavaApplication5.java
@@ -0,0 +1,68 @@
+﻿package javaapplication5;
+
+import java.math.BigInteger;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class JavaApplication5 {
+    
+    public static class MyThread extends Thread {
+    public int start;
+    public int end;
+    public BigInteger result;
+    public volatile boolean running;
+    public volatile boolean stopped;
+    public volatile boolean cancelled;
+    private static final int SLEEP_TIME=17;
+    
+    @Override
+    public void run(){
+        running = true;
+        stopped = false;
+        cancelled=false;
+        int current;
+        result=BigInteger.valueOf(0);
+        synchronized(myFrame){
+           start=myFrame.getNewStart();
+           end=myFrame.getNewEnd();
+        }
+        current=start;
+        while (current>0){
+            while (current<end){
+                if (!stopped){
+                    result=result.add(BigInteger.valueOf(current));
+                    current++;
+                }
+                if (cancelled) return;
+                try{
+                    Thread.sleep(SLEEP_TIME);
+                }
+                catch (InterruptedException qq) {
+                    qq.printStackTrace();
+                }
+            }
+            synchronized(myFrame){
+                myFrame.update();
+                start=myFrame.getNewStart();
+                end=myFrame.getNewEnd();
+            }
+            current=start;
+        }
+        running = false;
+    }
+
+}
+    
+    private static NewJFrame1 myFrame;
+    
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        myFrame = new NewJFrame1();
+        myFrame.setVisible(true);
+    }
+    
+}
---
+++ src/javaapplication5/NewJFrame1.java
@@ -0,0 +1,281 @@
+﻿package javaapplication5;
+
+import java.math.BigInteger;
+import javax.swing.JSpinner.NumberEditor;
+
+public class NewJFrame1 extends javax.swing.JFrame {
+
+    /**
+     * Creates new form NewJFrame1
+     */
+    public NewJFrame1() {
+        initComponents();
+        this.setTitle("Лабораторна робота №5");
+        jTextArea1.setLineWrap(true);
+        ((NumberEditor) jSpinner1.getEditor()).getTextField().setEditable(false);
+        ((NumberEditor) jSpinner2.getEditor()).getTextField().setEditable(false);
+        state=0;
+    }
+
+    @SuppressWarnings("unchecked")
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jSpinner1 = new javax.swing.JSpinner();
+        jSpinner2 = new javax.swing.JSpinner();
+        jLabel3 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+        jProgressBar1 = new javax.swing.JProgressBar();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setText("Formula 1+2+3+4+5+6+...");
+
+        jLabel2.setText("Number of iterations J:");
+
+        jSpinner1.setModel(new javax.swing.SpinnerNumberModel(10000, 100, 100000, 100));
+        jSpinner1.setEditor(new javax.swing.JSpinner.NumberEditor(jSpinner1, ""));
+
+        jSpinner2.setModel(new javax.swing.SpinnerNumberModel(10, 1, 100, 1));
+
+        jLabel3.setText("Number of threads T:");
+
+        jButton1.setText("Run parallel calculations");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jButton2.setText("Cancel calculations");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        jButton3.setText("Pause calculation");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+
+        jButton4.setText("Resume calculations");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 165, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel2)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jSpinner1, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel3)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jSpinner2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jButton4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(0, 12, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(29, 29, 29)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(jSpinner1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(jSpinner2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton3)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton4)
+                .addContainerGap(51, Short.MAX_VALUE))
+        );
+
+        jProgressBar1.setForeground(new java.awt.Color(255, 0, 255));
+
+        jTextArea1.setColumns(20);
+        jTextArea1.setRows(5);
+        jScrollPane1.setViewportView(jTextArea1);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jProgressBar1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 435, Short.MAX_VALUE)))
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jScrollPane1))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jProgressBar1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        pack();
+    }
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {
+        // TODO add your handling code here:
+        if (state!=0) return;
+        T = (Integer)jSpinner2.getValue();
+        curnum = 1-PACK_SIZE;
+        curdone=0;
+        max = (Integer)jSpinner1.getValue();
+        jProgressBar1.setMinimum(0);
+        jProgressBar1.setMaximum(max);
+        threadArray = new JavaApplication5.MyThread[T];
+        for (int i=0;i<T;i++){
+            threadArray[i] = new JavaApplication5.MyThread();
+            threadArray[i].setDaemon(true);
+            threadArray[i].start();
+        }
+        jTextArea1.append("\n\nStart of parallel calculations.\nNumber of threads: "+Integer.toString(T)+"\nTotal iterations: "+Integer.toString(max));
+        state=1;
+    }
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {
+        // TODO add your handling code here:
+        if (state!=1) return;
+        for (int i=0;i<T;i++) threadArray[i].stopped=true;
+        jTextArea1.append("\nThe calculation is paused");
+        state=2;
+    }
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {
+        // TODO add your handling code here:
+        if (state!=2) return;
+        for (int i=0;i<T;i++) threadArray[i].stopped=false;
+        jTextArea1.append("\nPayment restored ");
+        state=1;
+    }
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {
+        // TODO add your handling code here:
+        if (state==0) return;
+        for (int i=0;i<T;i++) threadArray[i].cancelled=true;
+        jTextArea1.append("\nThe calculation has been canceled");
+        jProgressBar1.setValue(0);
+        state=0;
+    }
+
+    private static final int PACK_SIZE = 5;
+    private int curnum;
+    private int max;
+    private int curdone;
+    private int T;
+    private int state;
+    JavaApplication5.MyThread[] threadArray;
+    
+    public void update(){
+        curdone+=PACK_SIZE;
+        jProgressBar1.setValue(curdone);
+        if (curdone>=max){
+            jTextArea1.append("\nCalculation complete.");
+            BigInteger totalsum = BigInteger.ZERO;
+            for (int i=0;i<T;i++){
+                totalsum=totalsum.add(threadArray[i].result);
+            }
+            jTextArea1.append("\nResult: "+totalsum.toString());
+            jTextArea1.append("\nThe result is by the formula "+BigInteger.valueOf(max+1).multiply(BigInteger.valueOf(max/2)).toString());
+            scroll();
+            state=0;
+        }
+    }
+    
+    private void scroll(){
+        jTextArea1.setSelectionStart(jTextArea1.getText().length());
+        jTextArea1.setSelectionEnd(jTextArea1.getText().length());
+    }
+    
+    public int getNewStart(){
+        curnum+=PACK_SIZE;
+        if (curnum<max) return curnum;
+        else return -1;
+    }
+    
+    public int getNewEnd(){
+        if (curnum+PACK_SIZE<max+1) return curnum+PACK_SIZE;
+        else return max+1;
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+             try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(NewJFrame1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(NewJFrame1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(NewJFrame1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(NewJFrame1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+             java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new NewJFrame1().setVisible(true);
+            }
+        });
+    }
+
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JProgressBar jProgressBar1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JSpinner jSpinner1;
+    private javax.swing.JSpinner jSpinner2;
+    private javax.swing.JTextArea jTextArea1;
+}
